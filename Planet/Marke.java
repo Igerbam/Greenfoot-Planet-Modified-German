d9095e5c1c15484dd7c83085a00effebdc41bc5d
Igerbam <87554520+Igerbam@users.noreply.github.com>
2021-12-08 12:07:01 +0100

    Delete Planet directory

--- Planet/Marke.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-/**
- * Die Klasse Marke ermöglicht Markierungen in der Welt.
- */
-public class Marke extends Actor {
-    public Marke() {
-        setImage("images/marke.png");
-    }
-}
